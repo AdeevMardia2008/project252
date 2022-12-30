19060362e2d36b655bf994e532db4a43a0c8e77d
AdeevMardia2008 <71126451+AdeevMardia2008@users.noreply.github.com>
2022-12-30 10:59:33 +0530

    Add files via upload

---
+++ key.py
@@ -0,0 +1,12 @@
+creds={
+  "type": "service_account",
+  "project_id": "project251-230df",
+  "private_key_id": "8d7f45515e9f9c285eca24be15bd1a45760b6674",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCnkm2YOwAeMsl4\nbQvscj4V36LqHVzjvXFAKjeL0yQmTiM/bTjJc60MkLD5tzcMWRFwblk6BE6iutmq\n5XJYnbshSVoS3CqmiT3Lxsg/FnDUweAZCyzc0rPqL/a1FGFg0e8qxaXR0uB62Faq\nx/tR08Pz7xnC04eHzpKF/oSGNaHHBM3Xuq55w6/ypZfT5Vecf1/mhdOHwkOIRNCn\n2wetCmQOQpCfY5LRuX7s05sQNGtgQQeaqmP4KiWk+LzEApVpohbS2+NXeLrHX411\nSDvhDQ6Pu7tMVstFCzWRiJRJ0nS2DvUqoATK1YTWkvHu0TkzS/IoNq3TV4KYyByK\nhSvQmtS9AgMBAAECggEAI4vOL7KkTcOcNS18kAWT4fP0jRD5eV3gPio2ajS5wF5m\n37d9ik49cSLoR7BzvuzN5DmkN8heHerg7X2Kif27ik4TC1kkuvr13tol9CwAd0FB\nFk7mj1kqzQyrmrHeDjm6MCqJCD9hWu21RLjzOOJg+MfJgSh54XcSYdYhqo6J6Ltl\nkVJph4rQc64+7kLiRhJb/22uKrzVko5CQgjPR4tFCESLaVqC1PjyVHU7tkbeQ2As\ncrt93qQQTvtjiMWU+FP7RdPYGuhiK9ycdQ/yfM0zQn1uWwxh9tNX77KDe5ikbkG9\nvmskvpjqUqF4XqD3TCYD/lp+NncysgY//kZ6ulgbgQKBgQDrOlmAAfgIwtQgYDh8\n1dB7MPb9gl8tYi8ycPgjfuVGSPgPqfLaT1F4GMIKADH4a4OLIBjsM6rCFbnQgs5W\nqPEykHUBZ2A3tvtXhmhFI6F9zhLIW2yLJLYDBOqtZH1wBskeFe6VnFWmMsWGvAXO\n2GGxT2ifJ8DIYm4pDf1w0/DHzQKBgQC2Xp97HES/aUu6eMLU8IJwcXd09+OdZZ4z\n4gyFYBNHw7cVDQydj1Zl+CHqa1XbeQ9AGVcVGYAugZZ02fOBDAasnSpWKDbpvUpQ\nWH5f6RGNvgBK1/fSY8emEDx3ha8s/wFDnRedL6XAAZw7uUU2CX+OcHVvjOJRAWkM\n/9rDCZNwsQKBgQDKAYUE2iwAkhgLabZ49kGg2a7yjHhiCBSSE9P036/TySxGf2NQ\nOZe0NEbGwd4oAWSpILpmdgRpwrCdMQg0AceI7eedegJ2NkzooEOBRJUV7dfRejnf\n0HoCY8GJgGf8wccLcDhBAU3IeYeKKoQuW0KiO5iUPVZgf68s0Jfawhff7QKBgBJ8\nRuggtms8jhuJsB4Lfv1M0xH9KopCyaVyrVg7o9irYvx0RSoXZGeTnkR3us0zWTNj\nHiuaWR7drdWMXh1fjbGjgK4ZB2/r6wN35N9t7YSTlM4BVDJ0QMF5ddebqmUCOCuV\nWdZqBQAQabi4D+bpUY+roIu9GRei7/YMXS1V5CcRAoGBAJRqJH8V/0boMuuaqu72\nR3MLghm+YQCxyZeJYVky0w2N9hLwWThok+fnt9ppFDGYLOyeLY7SXwwq3YsKuiGY\nvmAWtJV+aGXSWg787zCyY7S52D4XsT7RB46/DswLcBHq/SJYpjnn6sF+/BSgicBm\n+9w2lsQI1S2jGP3+hR++srsd\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-3j50l@project251-230df.iam.gserviceaccount.com",
+  "client_id": "109405648744570503045",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-3j50l%40project251-230df.iam.gserviceaccount.com"
+}
